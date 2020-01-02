634089f58a81d3f4cc039baacd643ef865b3ece4
jersey355 <ed.valdez@live.com>
2020-01-01 22:19:55 -0500

    Styled various components and pages

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import getTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
@@ -17,8 +18,13 @@ export class ExpensesSummary extends React.Component {
         const expWord = expCount === 1 ? 'expense' : 'expenses';
         const expTotal = numeral(getTotal(this.props.expenses) / 100).format('$0,0.00');
         return (
-            <div>
-                <h1>Viewing {expCount} {expWord} totalling {expTotal}</h1>
+            <div className="page-header">
+                <div className="content-container">
+                    <h1 className="page-header__title">Viewing <span>{expCount}</span> {expWord} totalling <span>{expTotal}</span></h1>
+                    <div className="page-header__actions">
+                        <Link className="button" to="/create">Add Expense</Link>
+                    </div>
+                </div>
             </div>
         );
     }
